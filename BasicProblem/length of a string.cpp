d9e9591f735bc50b55a3baaf068074b327d97912
emran-hossain <emran.green2037@gmail.com>
2024-12-12 17:19:54 +0600

    added a solution of finding length of a string

---
+++ BasicProblem/length of a string.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string text="a quick brown fox jump over the lazy dog";
+    //cout<<text.length();
+    //we can also use size() function to find the length of a string
+    cout<<text.size();
+    return 0;
+}
