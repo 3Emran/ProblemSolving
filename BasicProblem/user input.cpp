d7e26e5f0758307887748cef70c5d06405e2feb5
emran-hossain <emran.green2037@gmail.com>
2024-12-12 22:36:42 +0600

    added a solution of taking string user input

---
+++ BasicProblem/user input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string firstName,lastName;
+    cout<<"Enter Your First Name:";
+    cin >> firstName;
+    cout<<"Enter Your Last Name:";
+    cin >> lastName;
+    cout<<"Full Name:"<< firstName << " " << lastName <<endl;
+
+
+    return 0;
+}
