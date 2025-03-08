92aa04093de5280b88311a1adf0e3cdeb2d4040c
emran-hossain <emran.green2037@gmail.com>
2025-03-09 02:02:04 +0600

    Added a solution of a problem A-Election 2

---
+++ AtCoder/A - Election 2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int N,T,A;
+    cin>>N>>T>>A;
+    if(T >= (N+1)/2 || A >= (N+1)/2){
+        cout<<"Yes"<<endl;
+    }
+    else
+        cout<<"No"<<endl;
+    return 0;
+}
